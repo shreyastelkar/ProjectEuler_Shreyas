1ba0ebaab6f879608c1860cde7d9db8e25753624
SHREYASTELKAR <you@exa>
2024-02-14 17:42:31 -0800

    9

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -1,3 +1,4 @@
+from math import sqrt
 
 def mult_3and5():
     cur_sum = 0
@@ -20,12 +21,26 @@ def fibonacci():
     return sum
 
 
-def prime(num):
-    #prime- anything divisible by itself and 1,
-    tracking_primes = {}
-    for num in range(13195, 2, -1):
-        if not tracking_primes:
-            tracking_primes.update(num)
+def prime_fac():
+    # Example 20, 20 /2 = 10, 10 / 2 = 5, 
+    # 5 / 3, 
+    # 5 / 5 = 1, 
+    
+    num = 600851475143
+    startOdd = 3
+    
+    while (num % 2 == 0):
+        num /= 2
+    
+    while num != 1:
+        while (num % startOdd == 0):
+            last_num = num
+            num /= startOdd
+        startOdd += 2
+
+        
+    return int(last_num)
+    
             
 def three_digit_palindrome():
     cur = 0
@@ -58,21 +73,125 @@ def sum_square():
         c+=n**2
     return su - c
 
-def pythagorean():
-    pass
-
 def power_sum():
     s = 0
     for i in str(2**1000):
         s += int(i)
     return s
 
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def nthprime(n):
+    count = 0
+    num = 1
+    while count < n:
+        num += 1
+        if is_prime(num):
+            count += 1
+    return num
+        
+
+# 3^2 + 4^2 = 5^2, 9 + 16 = 25, 3 + 4 + 5 = 12
+# Find pythagorean triple where a + b + c = z
+
+def pythagorean():
+    
+    for a in range(1, 1000):
+        for b in range(a, 1000):
+            c_squared = a**2 + b**2
+            c = sqrt(c_squared)
+            if a + b + c == 1000:
+                return int(a * b * c)
+
+
+def str_to_matrix(inp):
+    matrix = []
+    start = 0
+    for row_i in range(20):
+        new_l = []
+        for col_i in range(start, start + 50):
+            new_l.append(int(inp[col_i]))
+        matrix.append(new_l)
+        start += 50
+    return matrix
+
+
+# need to check for last row where it should not go to the next line. 
+
+def adj_prod(inp, n):
+    matrix = str_to_matrix(inp)
+    cur_prod = 0
+    new_prod = 1
+    row_i = 0
+    i_step = 0
+    
+    while row_i < (len(matrix) - 1):
+        for col_i in range(0, len(matrix[row_i])): #0-49
+            
+            #with overlap
+            i_step = col_i
+            num = i_step + n
+            
+            while i_step < num:
+                
+                if (i_step//(len(matrix[row_i]))) > 0:
+                    r_i = row_i + 1
+                else:
+                    r_i = row_i
+                
+                print(i_step % (len(matrix[row_i])),r_i)
+                
+                new_prod *= matrix[r_i][i_step % (len(matrix[row_i]))]
+                i_step += 1
+
+            cur_prod = max(cur_prod, new_prod)
+            new_prod = 1
+            
+        row_i += 1
+        
+    return cur_prod
+
+def sum_primes():
+    
+
 
 if __name__=="__main__":
     
     #print(mult_3and5())
     # print(fibonacci())
+    #print(prime_fac())
     #print(three_digit_palindrome())
     #print(smallest_multiple())
    # print(sum_square())
     #print(power_sum())
+    print(nthprime(10001))
+    #print(pythagorean())
+    
+    inp = "73167176531330624919225119674426574742355349194934"\
+    "96983520312774506326239578318016984801869478851843"\
+    "85861560789112949495459501737958331952853208805511"\
+    "12540698747158523863050715693290963295227443043557"\
+    "66896648950445244523161731856403098711121722383113"\
+    "62229893423380308135336276614282806444486645238749"\
+    "30358907296290491560440772390713810515859307960866"\
+    "70172427121883998797908792274921901699720888093776"\
+    "65727333001053367881220235421809751254540594752243"\
+    "52584907711670556013604839586446706324415722155397"\
+    "53697817977846174064955149290862569321978468622482"\
+    "83972241375657056057490261407972968652414535100474"\
+    "82166370484403199890008895243450658541227588666881"\
+    "16427171479924442928230863465674813919123162824586"\
+    "17866458359124566529476545682848912883142607690042"\
+    "24219022671055626321111109370544217506941658960408"\
+    "07198403850962455444362981230987879927244284909188"\
+    "84580156166097919133875499200524063689912560717606"\
+    "05886116467109405077541002256983155200055935729725"\
+    "71636269561882670428252483600823257530420752963450"
+    
+    #print(adj_prod(inp, 13))
